c5b3b5f382841d1e008706500248256f9f085bae
Guangrui Wang <muma@fq-wifi-10-215-208-114.wireless.pitt.edu>
2021-09-23 16:37:12 -0400

    finished dropdown

---
+++ src/component/header/CusDropdown.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Dropdown } from "react-bootstrap";
+
+const CusDropdown = (props) => {
+  return (
+    <>
+      <Dropdown>
+        <Dropdown.Toggle variant="link" id="dropdown-basic">
+          <i className="fas fa-user" />
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">
+            <div>
+              <i className="fas fa-info-circle mr-2" /> Help Central
+            </div>
+          </Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="#/action-2">
+            <div>
+              <i className="fas fa-sign-in-alt " /> Log In | Sign Up
+            </div>
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
+  );
+};
+
+export default CusDropdown;
--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Navbar } from "react-bootstrap";
-import CusDropdown from "../layout/CusDropdown";
+import CusDropdown from "./CusDropdown";
 
 const Header = (props) => {
   return (
@@ -8,12 +8,14 @@ const Header = (props) => {
       <Navbar className="mx-5">
         <Navbar.Brand href="#home">METIS</Navbar.Brand>
         <Navbar.Toggle />
+        {/*/TODO add a search bar here*/}
+
         <Navbar.Collapse className="justify-content-end">
+          <CusDropdown />
           <Button variant="link" style={{ color: "black" }}>
             Login
           </Button>
           <Button variant="outline-secondary mx-2">Sign up</Button>
-          <CusDropdown />
         </Navbar.Collapse>
       </Navbar>
     </div>
