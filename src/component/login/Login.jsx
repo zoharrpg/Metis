9d475f93eedd8f87b42b42b1c302fa4c67b50f96
muma <guw18@pitt.edu>
2021-11-01 17:19:04 -0400

    add login todo

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Form, Row, Col } from "react-bootstrap";
 
 const Login = (props) => {
-  return <div>test</div>;
+  //TODO fix this login page
+  return (
+    <div className="container-fluid">
+      <Row>
+        <Col md={{ span: 4, offset: 1 }}>
+          <Form>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="2">
+                Email
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control
+                  plaintext
+                  readOnly
+                  defaultValue="email@example.com"
+                />
+              </Col>
+            </Form.Group>
+
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              controlId="formPlaintextPassword"
+            >
+              <Form.Label column sm="2">
+                Password
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control type="password" placeholder="Password" />
+              </Col>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Login;
