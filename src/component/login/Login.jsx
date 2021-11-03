538a90b4712375e475e11ad66303a976a2f50e99
Zohar <juj22@pitt.edu>
2021-11-03 12:14:49 -0400

    Complete the login form, but still has some problems about the position

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,41 +1,13 @@
 import React from "react";
-import { Form, Row, Col } from "react-bootstrap";
+
+import LoginForm from "./LoginForm";
+import "./Login.css";
 
 const Login = (props) => {
   //TODO fix this login page
   return (
-    <div className="container-fluid">
-      <Row>
-        <Col md={{ span: 4, offset: 1 }}>
-          <Form>
-            <Form.Group as={Row} className="mb-3">
-              <Form.Label column sm="2">
-                Email
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control
-                  plaintext
-                  readOnly
-                  defaultValue="email@example.com"
-                />
-              </Col>
-            </Form.Group>
-
-            <Form.Group
-              as={Row}
-              className="mb-3"
-              controlId="formPlaintextPassword"
-            >
-              <Form.Label column sm="2">
-                Password
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control type="password" placeholder="Password" />
-              </Col>
-            </Form.Group>
-          </Form>
-        </Col>
-      </Row>
+    <div className="vertical">
+      <LoginForm></LoginForm>
     </div>
   );
 };
